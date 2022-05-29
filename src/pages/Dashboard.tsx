e813b848882ba1a850c15789c3c3f8a3cc2f252b
Carlos Adriano Miranda <adriadrianooo@gmail.com>
2022-05-29 10:19:36 -0300

    favorite tab and list

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { TabPanels } from "../components";
+import FavoriteMovies from "./Favorites/Favorites";
 import NowPlaying from "./NowPlaying/NowPLaying";
 
 const Dashboard = () => {
   return (
     <div>
       <TabPanels>
-        <NowPlaying />
+        <NowPlaying key="1" />
+        <FavoriteMovies key="2" />
       </TabPanels>
     </div>
   );
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,47 @@
+import { useRef, useState } from "react";
+import { MovieResponse } from "../../types/global";
+import { SimpleGrid, GridItem } from "@chakra-ui/react";
+import MovieCard from "../../components/MovieCard";
+import MovieDetails, { ModalRelf } from "../../components/ModalMovieDetails";
+import { useMainContext } from "../../contexts/MainContext";
+
+const FavoriteMovies = () => {
+  const { favorites, setFavorites } = useMainContext();
+  const [movie, setMovie] = useState<MovieResponse | null>(null);
+  const refModal = useRef<ModalRelf>(null);
+
+  return (
+    <SimpleGrid
+      columns={{
+        sm: 1,
+        md: 2,
+        lg: 4,
+        xl: 6,
+        "2xl": 6,
+      }}
+      gap={4}
+    >
+      {favorites.map((movie) => {
+        return (
+          <GridItem>
+            <MovieCard
+              handleClickMovie={(_movie) => {
+                setMovie(_movie);
+                refModal.current?.onOpenModal();
+              }}
+              movie={movie}
+              isFavorite={false}
+            />
+          </GridItem>
+        );
+      })}
+      <MovieDetails
+        ref={refModal}
+        movie={movie}
+        handleCloseModal={() => setMovie(null)}
+      />
+    </SimpleGrid>
+  );
+};
+
+export default FavoriteMovies;
